e0ca67d028768c2d207e44dc9bde87994fcbfa7b
gadnlinoo <guiavenas@gmail.com>
2020-11-03 21:23:56 -0300

    Adicionando formato sugerido do arquivo de saída do simulador

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 from configuration import Configuration
+from models.frame import Frame
 
 file_buffer = open("config.json")
 
@@ -7,10 +8,12 @@ config_json = json.load(file_buffer)
 
 config = Configuration(**config_json)
 
-print(len(config.processes))
-print("-------------------------")
-for process in config.processes:
-	print(process.arrivalTime)
-	for inte in process.interruptions:
-		print("category={}, time = {}".format(inte.category, inte.time))
-	print("||||||")
+# print(len(config.processes))
+# print("-------------------------")
+# for process in config.processes:
+# 	print(process.arrivalTime)
+# 	for inte in process.interruptions:
+# 		print("category={}, time = {}".format(inte.category, inte.time))
+# 	print("||||||")
+
+print(json.dumps([Frame().data]))
---
+++ models/frame.py
@@ -0,0 +1,28 @@
+class Frame:
+    def __init__(self):
+        self.data = {
+            'pid': '',
+            'processor': {
+                'low': [],
+                'medium': [],
+                'high': []
+            },
+            'ioDevices': {
+                'magneticTape': [],
+                'printer': [],
+                'hardDisk': []
+            }
+        }
+
+    def set_pid(self, pid):
+        self.data['pid'] = pid
+
+    def set_processor_queues(self, lowQueue, mediumQueue, highQueue):
+        self.data['processor']['low'] = lowQueue
+        self.data['processor']['medium'] = mediumQueue
+        self.data['processor']['high'] = highQueue
+
+    def set_io_devices_queue(self, magneticTapeQueue, printerQueue, hardDiskQueue):
+        self.data['ioDevices']['magneticTape'] = magneticTapeQueue
+        self.data['ioDevices']['printer'] = printerQueue
+        self.data['ioDevices']['hardDisk'] = hardDiskQueue
