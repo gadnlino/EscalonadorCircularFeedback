df4a885b4b198b249d48be8cf8c6894698ec46ed
Gustavo Muzy Fraga <gumuzyfraga@gmail.com>
2020-11-04 23:02:26 -0300

    Add files via upload

--- models/linkedList.py
+++ models/linkedList.py
@@ -7,31 +7,44 @@ class Node(object):
     def getData(self):
         return self.data
 
+
 class LinkedList(object):
+
     def __init__(self, head=None):
         self.num = 0
         self.head = head
         self.tail = None
 
 
-    def enqueueEnd(self, data):
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+
+    def add(self, data):
         new_node = Node(data)
         if(self.num == 0):
             self.head = new_node
         else:
             self.tail.next = new_node
         self.tail = new_node
-        self.num+=1
+        self.num += 1
 
-    def dequeueBegin(self):
+    def pop(self):
         if(self.num > 0):
+            poppedNode = self.head
             self.head = self.head.next
             self.num -= 1
             if(self.num == 0):
                 self.tail = None
+            return poppedNode
+        return None
 
-    def __iter__(self):
-        node = self.head
-        while node:
-            yield node
-            node = node.next
+    def find(self, data):
+        for node in self: 
+            if(node == data): return True
+        return False
+			
+    def peek(self): 
+        return self.tail
